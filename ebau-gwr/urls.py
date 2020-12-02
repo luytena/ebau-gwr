784128de268ae24eb45896555216028ef6101733
Fabio Ambauen <fabio@dnmx.ch>
2020-12-02 09:17:06 +0100

    initial commit - cookiecutter

---
+++ ebau-gwr/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import include
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path("api-token-auth/", TokenObtainPairView.as_view(), name="login"),
+    path("api-token-refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("api/v1/", include("ebau-gwr.core.urls"),),
+]
---
+++ setup.py
@@ -0,0 +1,12 @@
+"""Setuptools package definition."""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="ebau-gwr",
+    version="0.0.0",
+    author="adfinis",
+    description="GWR synchronisation for ebau projects",
+    url="https://github.com/adfinis-sygroup/ebau-gwr",
+    packages=find_packages(),
+)
